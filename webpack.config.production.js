88fae2fc6ca51786fcc1a64db2379c17192ad663
oshepotkin <oleg.shepotkin@artezio.com>
2017-03-02 15:51:14 +0300

    webpack v2 prodaction optimize

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -6,29 +6,17 @@ var config = require('./webpack.config.base.js');
 var SaveAssetsJson = require('assets-webpack-plugin');
 
 config.bail = true;
-config.debug = false;
 config.profile = false;
 config.devtool = '#source-map';
 
 config.output = {
   path: './client/dist',
-  pathInfo: true,
   publicPath: '/client/dist/',
   filename: 'bundle.[hash].min.js'
 };
 
 config.plugins = config.plugins.concat([
-  new webpack.optimize.OccurenceOrderPlugin(true),
-  new webpack.optimize.DedupePlugin(),
-  new webpack.optimize.UglifyJsPlugin({
-    output: {
-      comments: false
-    },
-    compress: {
-      warnings: false,
-      screw_ie8: true
-    }
-  }),
+  new webpack.optimize.OccurrenceOrderPlugin(true),
   new SaveAssetsJson({
     path: process.cwd(),
     filename: 'assets.json'
